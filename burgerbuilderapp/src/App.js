545363ca614934a562a1e0d9d5830f66165eb876
wais <waisalmakaleh@gmail.com>
2020-01-11 17:54:25 -0500

    complete

--- burgerbuilderapp/src/App.js
+++ burgerbuilderapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter,Redirect } from "react-router-dom";
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
 import Layout from "./hoc/Layout/Layout";
@@ -15,23 +15,47 @@ class App extends Component {
 		this.props.onTryAutoSignup()
 	}
 	render() {
+
+		let routes = (
+			<Switch>
+				<Route path='/auth' exact component={ Auth } />
+				<Route path="/" component={ BurgerBuilder }/>
+				<Redirect to='/'/>
+			</Switch>
+				
+			)
+	
+		if (this.props.isAuthenticated) {
+			routes = (
+			<Switch>
+					<Route path='/checkout' component={ Checkout } />
+					<Route path='/orders' exact component={ Orders } />
+					<Route path='/logout' exact component={ Logout } />
+					<Route path="/" component={ BurgerBuilder } />
+					<Redirect to='/' />
+
+
+			</Switch>
+			)
+		}
 		return (
 			<div>
 				<Layout>
-					<Switch>
-						<Route path='/checkout' component={Checkout} />
-						<Route path='/orders' exact component={Orders} />
-						<Route path='/auth' exact component={Auth} />
-						<Route path='/logout' exact component={Logout} />
-						<Route path="/" component={ BurgerBuilder } />
-
-					</Switch>
+					
+				{routes}	
+					
 
 				</Layout>
 			</div>
 		);
 	}
 }
+
+const mapStateToProps = (state) => {
+	return {
+		isAuthenticated: state.auth.token !== null
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -40,4 +64,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
